dd4fe73c4832b605ebbc91fabf37d226a0836a49
Mateusz Krzyściak <103695808+Asterfekt@users.noreply.github.com>
2023-01-31 12:32:09 +0100

    recursion
    Silnia

---
+++ recursion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int factorial(int n)
+
+{
+        if(n==0)
+        return 1;
+        else
+    return n*factorial(n-1);
+}
+
+int main ()
+{
+    int n;
+
+    printf("Enter number: ");
+    scanf("%d",&n);
+
+    printf("n!=%d\n",factorial(n));
+
+    return 0;
+}
