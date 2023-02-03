9f6ca1ba592ab291d0944c4d03972b3225a8e209
Mateusz Krzyściak <103695808+Asterfekt@users.noreply.github.com>
2023-02-03 22:32:03 +0100

    Used Library Signal.h

---
+++ signals.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+
+void signal_handler(int sig)
+{
+    switch(sig)
+    {
+        case SIGFPE:
+            printf("Blad arytmetyczny : dzielenie przez 0 \n");
+            break;
+    }
+}
+int main(int argc, char** argv)
+{
+    int a, b, c;
+    signal(SIGFPE, signal_handler);
+    a = atoi(argv[1]);
+    b = atoi(argv[2]);
+    c = a / b;
+    printf("%d / %d = %d\n", a, b, c);
+    return 0;
+}
